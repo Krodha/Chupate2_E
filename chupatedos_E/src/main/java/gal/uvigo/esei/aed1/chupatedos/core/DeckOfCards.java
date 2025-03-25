f032d02e9dea59e4ed364609141a6d96e3ea1d07
rober <rober@robertopc>
2025-03-25 17:16:07 +0100

    Punto 4

--- chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/DeckOfCards.java
+++ chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/DeckOfCards.java
@@ -1,9 +1,29 @@
 package gal.uvigo.esei.aed1.chupatedos.core;
 
+import java.util.Stack;
+
 
 
 public class DeckOfCards {
-
+    private Stack<Card> stack;
+    
+    public void shuffle() {
+        Card[] cards = new Card[this.stack.size()];
+        int index = 0;
+        
+        while (!this.stack.isEmpty()) {
+            cards[index++] = this.stack.pop();
+        }
+        
+        for (int i = 0; i < cards.length; i++) {
+            cards[i] = cards[(int)Math.random() * cards.length - 1];
+        }
+        
+        for (int i = 0; i < cards.length; i++) {
+            this.stack.push(cards[i]);
+            
+        }
+    }
 
 
 }
