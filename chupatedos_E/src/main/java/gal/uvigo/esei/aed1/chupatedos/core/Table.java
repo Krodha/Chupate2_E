653f750a44dbe653e7e82ee3c3c267efdafd56ff
limag <limag@10.100.145.88>
2025-03-25 19:04:04 +0100

    punto5

--- chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Table.java
+++ chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Table.java
@@ -1,13 +1,30 @@
 package gal.uvigo.esei.aed1.chupatedos.core;
 
-
-
 public class Table {
 
+    private int numCardsTable;
+    private DeckOfCards deckofcards;
+    private int cartasDeJugadores = 0;
+    private Game game;
 
+    private int remainingCards() {
+        for (int i = 0; game.getNumOfPlayers() > i; i++) {
+            cartasDeJugadores = cartasDeJugadores + game.getPlayer(i).getCards().size();
+        }
+        return 40 - (numCardsTable + cartasDeJugadores);
+    }
 
-    
+    public int getNumCardsTable() {
+        return numCardsTable;
+    }
 
+    public Card getLastCard() {
+        return deckofcards.getTopCard();
+    }
 
-    
+    public Player playersHand() {
+        for (int i = 0; game.getNumOfPlayers() > i; i++) {
+            System.out.println(game.getPlayer(i).getName() + ": " + game.getPlayer(i).getCards() + "\n");
+        }
+    }
 }
