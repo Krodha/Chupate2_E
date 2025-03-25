191d4bc5085b68332a6ad2a7713e084fb8e2bf37
Lucas Cruces <lucascrucesfernandez@gmail.com>
2025-03-25 19:32:58 +0100

    Punto 1, v1.1

--- chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Game.java
+++ chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Game.java
@@ -2,48 +2,48 @@ package gal.uvigo.esei.aed1.chupatedos.core;
 
 import gal.uvigo.esei.aed1.chupatedos.iu.IU;
 
-
 public class Game {
+
     private final IU iu;
     private int numOfPlayers;
     private Player[] players;
-    
+    private DeckOfCards deckOfCards;
     
     public Game(IU iu) {
         this.iu = iu;
         this.numOfPlayers = 0;
         this.players = null;
-  }
-  
-   /**
-   * Metodo principal para jugar
-   */
-  public void play() {
-      do{
-      this.numOfPlayers = iu.readNumber("Cuantos jugadores van a jugar? (Entre 2 y 5)");
-      }while(this.numOfPlayers < 2 || this.numOfPlayers > 5);
-      players = new Player[this.numOfPlayers];
-      for(int i = 0; i <this.numOfPlayers; i++){
-          Player player = new Player(iu.readString("Nombre del jugador " + (i+1) + " :"));
-          this.players[i] = player;
-      }
+        this.deckOfCards = new DeckOfCards();
+    }
 
-      this.deckOfCards.shuffle();
-      
-      for (int i = 0; i < this.numOfPlayers; i++) {
-          for (int j = 0; j < 7; j++) {
-              this.players[i].addCard(this.deckOfCards.popCard());
-          }          
-      }
-      
-      iu.displayMessage("Carta sobre la mesa: " + deckOfCards.getTopCard());
+    /**
+     * Metodo principal para jugar
+     */
+    public void play() {
+        do {
+            this.numOfPlayers = iu.readNumber("Cuantos jugadores van a jugar? (Entre 2 y 5)");
+        } while (this.numOfPlayers < 2 || this.numOfPlayers > 5);
+        players = new Player[this.numOfPlayers];
+        for (int i = 0; i < this.numOfPlayers; i++) {
+            Player player = new Player(iu.readString("Nombre del jugador " + (i + 1) + " :"));
+            this.players[i] = player;
+        }
+
+        this.deckOfCards.shuffle();
+
+        for (int i = 0; i < this.numOfPlayers; i++) {
+            for (int j = 0; j < 7; j++) {
+                this.players[i].addCard(this.deckOfCards.popCard());
+            }
+        }
+
+        iu.displayMessage("Carta sobre la mesa: " + deckOfCards.getTopCard());
         iu.displayMessage("Número de cartas boca arriba: " + table.getNumCardsTable());
         iu.displayMessage("Cartas restantes sin repartir: " + table.remainingCards());
         iu.displayMessage("Cartas de los jugadores: " + table.playersHand());
 
-      // this.deckOfCards.getTopCard(); ToDo
-
-  }
+        // this.deckOfCards.getTopCard(); ToDo
+    }
 
     public int getNumOfPlayers() {
         return numOfPlayers;
@@ -52,11 +52,9 @@ public class Game {
     public Player[] getPlayers() {
         return players;
     }
-    
-    public Player getPlayer(int pos){
+
+    public Player getPlayer(int pos) {
         return players[pos];
     }
-
- 
 
 }
