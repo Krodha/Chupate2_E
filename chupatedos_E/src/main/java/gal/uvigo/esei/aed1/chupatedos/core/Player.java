59b56607895276706c04a09922399376c0fca0c6
adrit <Adrin>
2025-03-25 18:07:07 +0100

    Punto 2

--- chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Player.java
+++ chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Player.java
@@ -1,8 +1,29 @@
 package gal.uvigo.esei.aed1.chupatedos.core;
 
+import java.util.Stack;
+
 
 
 public class Player {
+    private String nombre;
+    private Stack <Card> cards;
+    
+ 
+    public Player(String nombre){
+        this.nombre = nombre;
+        this.cards = new LinkedStack<>();
+    }
 
+    public Stack<Card> getCards() {
+        return cards;
+    }
+    
+    public void addCard(Card card){
+        if(cards.size() >= 7){
+            throw new IllegalArgumentException("The stack is full. Can't add more cards");
+        } 
+        cards.push(card);
+    }
+    
     
 }
