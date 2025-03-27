9a6a36a540540e4300e36f1fa231c4bfb8633ee2
limag <limag@10.100.145.88>
2025-03-27 18:46:07 +0100

    Cambio showCards y ENTREGA1 HECHA v2

--- chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Player.java
+++ chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Player.java
@@ -3,15 +3,12 @@ package gal.uvigo.esei.aed1.chupatedos.core;
 import es.uvigo.esei.aed1.tads.stack.Stack;
 import es.uvigo.esei.aed1.tads.stack.LinkedStack;//CAMBIO
 
-
-
 public class Player {
+
     private String name;
-    private Stack <Card> cards;
-    
-    
- 
-    public Player(String name){
+    private Stack<Card> cards;
+
+    public Player(String name) {
         this.name = name;
         this.cards = new LinkedStack<>();
     }
@@ -23,14 +20,28 @@ public class Player {
     public String getName() {
         return name;
     }
-    
-    
-    public void addCard(Card card){
-        if(cards.size() >= 7){
+
+    public void addCard(Card card) {
+        if (cards.size() >= 7) {
             throw new IllegalArgumentException("The stack is full. Can't add more cards");
-        } 
+        }
         cards.push(card);
     }
-    
-    
+
+    public String showCards() {
+        Stack<Card> aux = new LinkedStack<>();
+        StringBuilder sb = new StringBuilder();
+
+        while (!cards.isEmpty()) {
+            aux.push(cards.pop());
+        }
+
+        while (!aux.isEmpty()) {
+            sb.append(aux.top().toString()).append(", ");
+            cards.push(aux.pop());
+        }
+
+        return sb.toString();
+    }
+
 }
