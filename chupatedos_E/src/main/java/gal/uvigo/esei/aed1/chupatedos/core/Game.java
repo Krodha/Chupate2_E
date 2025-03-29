90d1a70d4a18ad61f667d0498dcb19565d903497
Roberto Diz <robertodizp@gmail.com>
2025-03-29 13:37:35 +0100

    Fix show face-up card

--- chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Game.java
+++ chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Game.java
@@ -41,7 +41,7 @@ public class Game {
 
         this.table.setFaceUpCard(this.deckOfCards.popCard());
 
-        iu.displayMessage("Carta sobre la mesa: " + deckOfCards.getTopCard());
+        iu.displayMessage("Carta sobre la mesa: " + this.table.getFaceUpCard());
         iu.displayMessage("Número de cartas boca arriba: " + table.getNumCardsTable());
         iu.displayMessage("Cartas restantes sin repartir: " + deckOfCards.getSize());
         iu.displayMessage("Cartas de los jugadores:\n" + playersHand());
