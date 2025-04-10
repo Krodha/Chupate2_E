eb86bc53bb0a4a19e5a171ad8c171efdff213387
Lucas Cruces <lucascrucesfernandez@gmail.com>
2025-04-10 17:32:43 +0200

    Player  -->  getPlayableCards

--- chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Player.java
+++ chupatedos_E/src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Player.java
@@ -21,13 +21,23 @@ public class Player {
     public void addCard(Card card) {
         cards.addLast(card);
     }
-
+    
+    public List<Card> getPlayableCards(Card cardToPlay){
+        List<Card> playableCards = new LinkedList<>();
+        
+        for(Card crd : cards){
+            if (crd.getSuit().equals(cardToPlay.getSuit()) || crd.getNumber() == cardToPlay.getNumber()) {
+                playableCards.addLast(crd);
+            }
+        }
+        return playableCards;
+    }
     
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(name).append(": \n\t");
+        sb.append(name).append(":\n\t");
         
         for(Card crd : cards){
             sb.append(crd.toString());
